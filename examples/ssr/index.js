8a80117e686bfe1fae207eb9b97dd7ffb6eea5e1
Youssef <youssef.jsdeveloper@gmail.com>
2019-05-28 15:25:17 +0430

    Add more component to examples

--- examples/ssr/index.js
+++ examples/ssr/index.js
@@ -1,19 +1,26 @@
-const { createElement } = require('react')
+const { createElement, Fragment } = require('react')
 const { renderToString } = require('react-dom/server')
 const express = require('express')
-const { Ellipsis } = require('test-repo')
+const { Ellipsis, Ring, Ripple } = require('test-repo')
 
 const port = 3000
 const app = express()
 
 app.get('*', (req, res) => {
-  const el = createElement(Ellipsis)
-
-  const html = renderToString(el)
+  const html = renderToString(
+    createElement(
+      Fragment,
+      null,
+      createElement(Ellipsis),
+      createElement(Ring),
+      createElement(Ripple)
+    )
+  )
   res.send(`<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8"/>
+        <title>CJS Example</title>
     </head>
     <body>
         ${html}
